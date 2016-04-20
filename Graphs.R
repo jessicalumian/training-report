6e7ca09d6895ea478a773c39cc041914fe84baf1
jessicamizzi <jessica.mizzi@gmail.com>
2016-04-20 11:12:49 -0700

    refine graph script, add better graph

--- Graphs.R
+++ Graphs.R
@@ -1,17 +1,19 @@
-workshopdata = as.data.frame(read.csv("~/Downloads/workshop_overviews.csv"))
+workshopdata = as.data.frame(read.csv("~/Downloads/workshop_overview_simple_mdy.csv"))
 
 # Graph attendance of workshops over time. x axis is time, y is attendance. if possible,
 # color of bars correspond to type of workshop (half day, two day, etc)
 
 library(ggplot2)
+library(scales)
 # define variables for graph from data frame
-workshopdata$date = as.factor(workshopdata$Date)
+workshopdata$date = as.Date(workshopdata$Date)
 workshopdata$signups = as.numeric(workshopdata$Sign.ups.on.EventBrite)
 workshopdata$Format = as.factor(workshopdata$Format)
 
 plot <- ggplot(data=workshopdata, aes(x=date, y=signups, fill=Format)) +
-  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+   geom_bar(stat="identity") +
+   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-plot <- plot + labs(title = "Workshop Attendance Over Time")
 
-plot + labs(x = "Workshop Date", y = "Workshop Registrants")
+plot <- plot + labs(title = "Workshop Attendance Over Time") + theme(legend.position = c(0.9, 0.8))
+plot + aes(width=7) + labs(x = "Workshop Date", y = "Workshop Registrants") + scale_x_date(breaks = pretty_breaks(10))

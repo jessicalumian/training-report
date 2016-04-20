3c5d1968148c3fd8e424bbbcee5b94bef07eca31
jessicamizzi <jessica.mizzi@gmail.com>
2016-04-20 09:42:16 -0700

    refine graph script, add graph pic

--- Graphs.R
+++ Graphs.R
@@ -7,7 +7,11 @@ library(ggplot2)
 # define variables for graph from data frame
 workshopdata$date = as.factor(workshopdata$Date)
 workshopdata$signups = as.numeric(workshopdata$Sign.ups.on.EventBrite)
-workshopdata$format = as.factor(workshopdata$Format)
+workshopdata$Format = as.factor(workshopdata$Format)
 
-ggplot(data=workshopdata, aes(x=date, y=signups, fill=format)) +
-  geom_bar(stat="identity")
+plot <- ggplot(data=workshopdata, aes(x=date, y=signups, fill=Format)) +
+  geom_bar(stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+plot <- plot + labs(title = "Workshop Attendance Over Time")
+
+plot + labs(x = "Workshop Date", y = "Workshop Registrants")
